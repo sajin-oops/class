854cfaa838084228c0419da140d0fd03a5da0bae
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-10 19:55:52 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,9 @@
+# Create a class named MyClass, with a property named x:
+
+class myfirstclass:
+  x = 5
+#Create an object named p1, and print the value of x:
+obj = myfirstclass()
+print(obj.x)
+
+#O/P - 5
